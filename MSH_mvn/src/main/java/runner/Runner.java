8d13581017e30eb76256d56b37c6ad33fd264787
wuliZXF <1094013497@qq.com>
2016-11-20 14:32:33 +0800

    1

---
+++ MSH_mvn/src/main/java/runner/Runner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import javafx.application.*;
+import javafx.stage.*;
+
+public class Runner extends Application{
+
+	@Override
+	public void start(Stage primary) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- MSH_mvn/src/main/java/ui/main_ui/main.java
+++
@@ -1,7 +0,0 @@
-package ui.main_ui;
-
-public class main {
-	public static void main(String[] args){
-		
-	}
-}
