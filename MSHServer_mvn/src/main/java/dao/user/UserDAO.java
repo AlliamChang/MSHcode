2385a6f80967418cbaf20f6cbdd446500b70f425
wuliZXF <1094013497@qq.com>
2016-12-14 14:07:54 +0800

    1

---
+++ MSHServer_mvn/src/main/java/dao/user/UserDAO.java
@@ -0,0 +1,29 @@
+package dao.user;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+import po.UserPO;
+import tools.ResultMessage;
+
+public interface UserDAO extends Remote {
+
+	public UserPO getUser(String account) throws RemoteException;
+	
+	public UserPO getUser(int ID) throws RemoteException;
+	
+	public List<UserPO> getAllMarketers() throws RemoteException;
+
+	/**
+	 * 添加新用户，返回系统分配ID
+	 * @param userPO
+	 * @return
+	 * @throws RemoteException
+	 */
+	public int addUser(UserPO userPO) throws RemoteException;
+	
+	public ResultMessage modifyUser(UserPO userPO) throws RemoteException;
+
+	public ResultMessage deleteUser(int ID) throws RemoteException;
+}
